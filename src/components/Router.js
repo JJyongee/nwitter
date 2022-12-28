c9a3682e0601ca67613d55c7162d9789351d21c7
jjyongee <wldyd010110@gmail.com>
2022-12-28 14:25:27 +0900

    수정

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,8 +1,8 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
 
-const AppRouter = ({isLoggedIn}) => {
+const AppRouter = ({ isLoggedIn }) => {
     return (
         <Router>
             <Routes>
